178d80908e672c83661a3f53d934b370a7ac0ea9
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2019-02-24 13:03:56 +0000

    Added basic function and some variables

--- SHA256.c
+++ SHA256.c
@@ -1,8 +1,38 @@
+// Author: Edward Eldridge 
+// Program: SHA-256 Algorithm implentation in C
+// Resources: https://github.com/EddieEldridge/SHA256-in-C/blob/master/README.md
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
- int main(int argc, char *argv[]) 
- {
+void sha256();
+
+// Main method
+int main(int argc, char *argv[]) 
+{
+     // Print header
      printf("\n======== SHA256 - HASHING ALGORITHM ========\n");
- }
+
+     // Function calls
+     sha256();
+    
+     // Main return method
+     return 0;
+}
+
+void sha256()
+{
+    // Message schedule
+    __uint32_t W[64];
+
+    // Working variables
+    __uint32_t a, b, c, d, e, f, g, h;
+
+    // Hash value
+    __uint32_t H[8];
+
+    // Temp variables
+    __uint32_t T1;
+    __uint32_t T2;
+}
