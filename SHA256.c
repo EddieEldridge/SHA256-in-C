feae3296365efe8569d6de35db196fd02ba141bd
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2019-02-24 13:07:42 +0000

    Added intial hash values

--- SHA256.c
+++ SHA256.c
@@ -30,7 +30,16 @@ void sha256()
     __uint32_t a, b, c, d, e, f, g, h;
 
     // Hash value
-    __uint32_t H[8];
+    __uint32_t H[8] = {
+        0x6a09e667,
+        0xbb67ae85,
+        0x3c6ef372,
+        0xa54ff53a,
+        0x510e527f,
+        0x9b05688c,
+        0x1f83d9ab,
+        0x5be0cd19,
+    };
 
     // Temp variables
     __uint32_t T1;
