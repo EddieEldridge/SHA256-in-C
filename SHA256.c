41980c9f7401205f73f1a5e4dddb06493590870d
EddieEldridge <steadyeddie101@hotmail.co.uk>
2019-03-31 12:49:00 +0100

    Hash still not working

--- SHA256.c
+++ SHA256.c
@@ -269,7 +269,7 @@ int fillMessageBlock(FILE *file, union messageBlock *msgBlock, enum status *stat
         }
 
         // Set the last 64 bits to an integer (should be big endian)
-        msgBlock->s[7] = *numBits;
+        msgBlock->s[7] = byteSwap64(*numBits);
 
         // Set the state to finish
         *state = FINISH;
@@ -409,7 +409,7 @@ int endianCheck()
 
 // Reference - http://www.firmcodes.com/write-c-program-convert-little-endian-big-endian-integer/
 // Converts a little endian integer to big endian and vice versa (32 bit version)
-__uint64_t byteSwap32(__uint64_t x)
+__uint32_t byteSwap32(__uint32_t x)
 {
     return (((x>>24) & 0x000000ff) | ((x>>8) & 0x0000ff00) | ((x<<8) & 0x00ff0000) | ((x<<24) & 0xff000000));
 }
@@ -417,9 +417,9 @@ __uint64_t byteSwap32(__uint64_t x)
 
 // Reference - https://stackoverflow.com/questions/21507678/reverse-bytes-for-64-bit-value
 // Converts a little endian integer to big endian and vice versa (64 bit version)
-__uint64_t byteSwap64(__uint64_t x)
+__uint64_t byteSwap64(__uint64_t integer)
 {
-    uint64_t x = (uint64_t) x;
+    uint64_t x = (uint64_t) integer;
     x = (x & 0x00000000FFFFFFFF) << 32 | (x & 0xFFFFFFFF00000000) >> 32;
     x = (x & 0x0000FFFF0000FFFF) << 16 | (x & 0xFFFF0000FFFF0000) >> 16;
     x = (x & 0x00FF00FF00FF00FF) << 8  | (x & 0xFF00FF00FF00FF00) >> 8;
